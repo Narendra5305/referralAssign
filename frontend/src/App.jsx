1a073e60ce47e345017ea2120536fead705a8a7d
Narendra Singh <narendrasinghsitu4@gmail.com>
2025-07-10 18:09:01 +0530

    fix error for deployement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // import "./app.css"
 
-import Login from './pages/Login';
+import Login from './pages/login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import ReferCandidate from './pages/ReferCandidate';
